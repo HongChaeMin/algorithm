d26d33732bed3ff712e3aac1200fbcc713db9063
HongChaeMin <ghdcoalss33@gmail.com>
2022-02-12 17:52:44 +0900

    [minHong] baekjoon 10171

---
+++ src/com/mycompany/algorithm/baekjoon/Quiz10171.java
@@ -0,0 +1,14 @@
+package com.mycompany.algorithm.baekjoon;
+
+public class Quiz10171 {
+
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+
+}
