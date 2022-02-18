31406cea77fa2a5dc313b743ffc22f12c94207c9
HongChaeMin <ghdcoalss33@gmail.com>
2022-02-18 21:52:18 +0900

    [minHong] baekjoon 11022

---
+++ src/com/mycompany/algorithm/baekjoon/Quiz11022.java
@@ -0,0 +1,30 @@
+package com.mycompany.algorithm.baekjoon;
+
+import java.util.Scanner;
+
+public class Quiz11022 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int x = 1;
+
+
+        int arr[] = new int[n * 2];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                System.out.println("Case #" + x + ": " + arr[i] + " + " + arr[i + 1] + " = " + (arr[i] + arr[i + 1]));
+                x++;
+            }
+        }
+
+    }
+}
+
