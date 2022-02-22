139e733c015c86d35f3345c8a6bd2e4f95d91353
홍채민 <coals0329@cocen.com>
2022-02-22 12:12:51 +0900

    [minHong] baekjoon quiz 2

---
+++ src/com/mycompany/algorithm/baekjoon/Quiz15596.java
@@ -0,0 +1,19 @@
+package com.mycompany.algorithm.baekjoon;
+
+public class Quiz15596 {
+
+    long sum(int[] a) {
+        long ans = 0;
+
+        for (int i : a) {
+            ans += i;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
