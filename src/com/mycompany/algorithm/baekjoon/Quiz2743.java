54f97e705b1f450f764ef7e4362fcc6609a2bc0a
HongChaeMin <ghdcoalss33@gmail.com>
2022-03-01 20:59:45 +0900

    [minHong] baekjoon 2743

---
+++ src/com/mycompany/algorithm/baekjoon/Quiz2743.java
@@ -0,0 +1,17 @@
+package com.mycompany.algorithm.baekjoon;
+
+import java.util.Scanner;
+
+public class Quiz2743 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        System.out.println(str.length());
+
+    }
+
+}
