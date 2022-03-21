d5dd27cbf7631859f05c6ff1528f6aa5e60ec4ca
minHong <57277976+HongChaeMin@users.noreply.github.com>
2022-03-21 22:15:52 +0900

    [minHong] baekjoon 2739 (#29)
    Co-authored-by: 홍채민 <coals0329@cocen.com>

---
+++ src/com/mycompany/algorithm/baekjoon/Quiz2739.py
@@ -0,0 +1,7 @@
+number1 = int(input())
+number2 = int(input())
+
+print(number1 * int(number2 / 100))
+print(number1 * int(number2 / 10))
+print(number1 * int(number2 / 1))
+print(number1 * number2)
