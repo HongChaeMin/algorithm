2ba730af76c1f7021b5fce0199011ea47d81534e
minHong <57277976+HongChaeMin@users.noreply.github.com>
2022-04-21 10:50:37 +0900

    Min hong (#35)
    * feat(baekjoon): Quiz 14681
    
    * feat(baekjoon): Quiz 10818
    
    * feat(programmers): 신고결과 받기 - 리펙터링중
    
    * feat(programmers): 신고 결과 받기 & add gitignore
    
    Co-authored-by: 홍채민 <coals0329@cocen.com>

--- src/com/mycompany/algorithm/progammers/Solution.java
+++ src/com/mycompany/algorithm/progammers/Solution.java
@@ -7,20 +7,40 @@ public class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         List<Person> personList = makePersonList(id_list);
 
-
-        // 신고된 사람, 신고한 사람 분리
+        // 중복 제거
         Set<String> setList = new HashSet<>(Arrays.asList(report));
 
+        // 신고자, 신고받은 사람 분리
         List<String> reporterList = new ArrayList<>();
         List<String> personReportedList = new ArrayList<>();
 
-        for (String names : setList) {
-            String[] declaration = names.split(" ");
-            reporterList.add(declaration[0]);
-            personReportedList.add(declaration[1]);
-        }
+        dissociation(setList, reporterList, personReportedList);
 
-        // 신고를 한 사람의 이름과 신고당한 횟수 계산
+        // 신고를 한 사람의 이름 저장과 신고당한 횟수 계산
+        reporterSaveNameAndReportCount(personList, reporterList, personReportedList);
+
+        // 메일 받는 횟수 계산
+        receiveMailCount(personList, k);
+
+        return makeAnswer(id_list.length, personList);
+    }
+
+    public void receiveMailCount(List<Person> personList, int limitCount) {
+        for (int i = 0; i < personList.size(); i++) {
+            for (Person person : personList) {
+                if (isLimitCount(personList, person, i, limitCount)) {
+                    person.addReceiveMailCount();
+                }
+            }
+        }
+    }
+
+    public boolean isLimitCount(List<Person> personList, Person person, int idx, int limitCount) {
+        return personList.get(idx).getReportedCount() >= limitCount && person.getReportedPersonNameList().contains(personList.get(idx).getPersonName());
+    }
+
+
+    public void reporterSaveNameAndReportCount(List<Person> personList, List<String> reporterList, List<String> personReportedList) {
         for (Person person : personList) {
             for (int j = 0; j < reporterList.size(); j++) {
                 // 신고한 사람 이름 넣기
@@ -33,17 +53,14 @@ public class Solution {
                 }
             }
         }
+    }
 
-        // 메일 받는 횟수 계산
-        for (int i = 0; i < personList.size(); i++) {
-            for (Person person : personList) {
-                if (personList.get(i).getReportedCount() >= k && person.getReportedPersonNameList().contains(personList.get(i).getPersonName())) {
-                    person.addReceiveMailCount();
-                }
-            }
+    public void dissociation(Set<String> setList, List<String> reporterList, List<String> personReportedList) {
+        for (String names : setList) {
+            String[] declaration = names.split(" ");
+            reporterList.add(declaration[0]);
+            personReportedList.add(declaration[1]);
         }
-
-        return makeAnswer(id_list.length, personList);
     }
 
     public List<Person> makePersonList(String[] id_list) {
